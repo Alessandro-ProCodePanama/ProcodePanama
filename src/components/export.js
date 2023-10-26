c36a523c6501ba0dab94e0738ff90b445bf31d4e
Alessandro-ProCodePanama <148911488+Alessandro-ProCodePanama@users.noreply.github.com>
2023-10-26 16:51:28 -0500

    Delete src directory

--- src/components/export.js
+++
@@ -1,7 +0,0 @@
-import Navbar from "./Navbar"
-import HomeText from "./Text"
-import Nosotros from "./Nosotros"
-import MV from "./MV"
-import Servicios from "./Servicios"
-
-export { Navbar, HomeText, MV, Nosotros, Servicios }
